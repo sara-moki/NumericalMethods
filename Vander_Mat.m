653d9bf3d5990e293ae43846fc34e3649376a238
ساره <99841907+sara-moki@users.noreply.github.com>
2022-03-29 00:39:56 +0300

    Update Vander_Mat.m

--- Vander_Mat.m
+++ Vander_Mat.m
@@ -1,4 +1,7 @@
 function V_Matrix = Vander_Mat(x)
+%Input: x is a vector s.t. x =[x_0, x_1,...,x_n]
+%Output: Vandermonde matrix 
+% Written on : 14\03\2022
 n = length(x);
 % V = zeros(n);
 % V(:,1) = 1;
@@ -6,6 +9,7 @@ n = length(x);
 %         V(:,i+1) = x.^i;
 % end
 % V_Matrix = V;
+%or :
 V=[];
 for i=1:n
         V = [V , x(:).^(i-1)];
